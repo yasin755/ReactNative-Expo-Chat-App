057e53869035ee7ff7f274d6ac7dec82c8c21730
yasin <myasin@xemplarinsights.com>
2025-09-10 22:23:06 +0530

    api url modified

--- Chat-App/helper/apiServices.ts
+++ Chat-App/helper/apiServices.ts
@@ -4,9 +4,9 @@ import { router } from "expo-router";
 
 //const apiURL = "http://192.168.1.78:8000";
 //const apiURL = "http://localhost:8000";
-const apiURL = "http://192.168.1.34:8000";
+//const apiURL = "http://192.168.1.34:8000";
+const apiURL = "https://reactnative-expo-chat-app.onrender.com";
 const baseUrl = `${apiURL}/api`;
-
 
 const myAxios: AxiosInstance = axios.create({
   baseURL: baseUrl,
