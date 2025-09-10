7801f7ec19674d656b6452b52980e6b05de43858
yasin <myasin@xemplarinsights.com>
2025-09-10 22:25:04 +0530

    db config updated

--- Backend/database/dbConfig.js
+++ Backend/database/dbConfig.js
@@ -2,13 +2,25 @@
 import { Sequelize, DataTypes, Op } from "sequelize";
 
 // Create a connection pool (better than single connection)
-const sequelize = new Sequelize({
+// Dev
+/*const sequelize = new Sequelize({
   host: "localhost", // MySQL server host
   username: "root", // your MySQL username
   password: "Mahira9*", // replace with your MySQL password
   database: "chatdb", // the DB you created
   dialect: "mysql",
   port: 3306,
+  logging: false,
+});*/
+
+// Prod
+const sequelize = new Sequelize({
+  host: "chatappdb-chat-app755.d.aivencloud.com", // MySQL server host
+  username: "avnadmin", // your MySQL username
+  password: process.env.AIVEN_PASSWORD, // replace with your MySQL password
+  database: "chatdb", // the DB you created
+  dialect: "mysql",
+  port: 10346,
   logging: false,
 });
 
